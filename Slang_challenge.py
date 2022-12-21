bc21c1b5ced8608bff0951da1e119e0370a05761
Juan Munar <juan_munarb@hotmail.com>
2022-12-21 16:06:43 -0500

    Success fetching data

--- Slang_challenge.py
+++ Slang_challenge.py
@@ -1,2 +1,14 @@
-import pip._vendor.requests
+# Imported Modules
+from pip._vendor import requests
 from datetime import datetime, timedelta
+
+# Set up the API
+api_endpoint = "https://api.slangapp.com/challenges/v1/activities"
+headers = {"Content-Type": "application/json", "Authorization": "Basic MTUyOlVrelczQ2JBaXlaVTNLWTFFVVhPbnhHSTdvaEhpQkVvdURqeEZITHJzUzA9"}
+
+# Fetch data from the API
+response = requests.get(api_endpoint, headers=headers)
+
+# Check for success 
+if response.status_code == 200:
+    print('success')
